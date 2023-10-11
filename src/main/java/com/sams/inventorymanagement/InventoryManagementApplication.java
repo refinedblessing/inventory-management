ea4dc9448decbf51560a886acfe90c6e42665705
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-10-11 15:49:11 -0700

    Project set up

---
+++ src/main/java/com/sams/inventorymanagement/InventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sams.inventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sams/inventorymanagement/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sams.inventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
