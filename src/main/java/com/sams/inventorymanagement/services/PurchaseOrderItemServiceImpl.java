e28ea56000c46fae1dac5fbbd4865b9984ffee41
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-10-17 12:26:46 -0700

    Add check/fix for when a product order item quantity exceeds warehouse quantity

--- src/main/java/com/sams/inventorymanagement/services/PurchaseOrderItemServiceImpl.java
+++ src/main/java/com/sams/inventorymanagement/services/PurchaseOrderItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sams.inventorymanagement.services;
 
+import com.sams.inventorymanagement.entities.PurchaseOrder;
 import com.sams.inventorymanagement.entities.PurchaseOrderItem;
 import com.sams.inventorymanagement.repositories.PurchaseOrderItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,23 @@ public class PurchaseOrderItemServiceImpl implements PurchaseOrderItemService {
 
     @Override
     public PurchaseOrderItem createPurchaseOrderItem(PurchaseOrderItem purchaseOrderItem) {
+        resetQuantityToMaxAvailableQuantity(purchaseOrderItem);
         return purchaseOrderItemRepository.save(purchaseOrderItem);
+    }
+
+    /**
+     * Resets the quantity of a PurchaseOrderItem to the maximum available quantity.
+     * This method ensures that the quantity of a PurchaseOrderItem is set to the maximum
+     * available quantity, preventing over-ordering when creating a purchase order.
+     *
+     * @param purchaseOrderItem The PurchaseOrderItem to reset the quantity for.
+     */
+    private void resetQuantityToMaxAvailableQuantity(PurchaseOrderItem purchaseOrderItem) {
+        int availableQuantity = purchaseOrderItem.getItem().getQuantity();
+        int quantity = purchaseOrderItem.getQuantity();
+
+        // Ensure that the quantity does not exceed the available quantity.
+        purchaseOrderItem.setQuantity(Math.min(quantity, availableQuantity));
     }
 
     @Override
@@ -43,6 +60,7 @@ public class PurchaseOrderItemServiceImpl implements PurchaseOrderItemService {
     public PurchaseOrderItem updatePurchaseOrderItem(Long id, PurchaseOrderItem updatedPurchaseOrderItem) {
         if (purchaseOrderItemRepository.existsById(id)) {
             updatedPurchaseOrderItem.setId(id); // Ensure the ID is set
+            resetQuantityToMaxAvailableQuantity(updatedPurchaseOrderItem);
             return purchaseOrderItemRepository.save(updatedPurchaseOrderItem);
         } else {
             return null; // Return null if the item with the specified ID does not exist
