49fbe8b9ce0e4223ff5d8a90859aa262bf45618b
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-10-22 15:42:30 -0700

    Heroku setup

---
+++ src/main/java/com/sams/inventorymanagement/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.sams.inventorymanagement.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<Object> home() {
+        return new ResponseEntity<>("Welcome Home", HttpStatus.OK);
+    }
+}
